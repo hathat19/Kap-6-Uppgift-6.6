fc10862b3c2004fd28e182d7322220c4bb820629
Hugo M <humy06001@utb.vaxjo.se>
2024-02-14 13:30:46 +0100

    Add project files.

---
+++ Kap 6 Uppgift 6.6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Färger
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+            //-------------------------------------------
+
+
+            //SKRIV MAIN-KOD HÄR
+            UtskriftVarannan("hej jag heter hugo");
+            UtskriftVarannan("håkon luktar mycket illa");
+        }
+
+        static void UtskriftVarannan(string utskrift)
+        {
+            string[] ord = utskrift.Split(' ');
+            for (int i = 0; i < ord.Length; i++) 
+            { 
+                string tillfälligtOrd = i % 2 == 0 ? ord[i].ToUpper() : ord[i].ToLower();
+                Console.Write(tillfälligtOrd + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+/*Skapa ett program som innehåller en metod som tar en sträng som parameter och
+skriver ut strängen fast vartannat ord i strängen ska skrivas ut med enbart stora
+bokstäver och vartannat ord med enbart små. Om metoden anropas med
+argumentet Detta är ett exempel. Texten ändras när den skrivs ut. så ska utskriften bli DETTA
+är ETT exempel. TEXTEN ändras NÄR den SKRIVS ut.*/
